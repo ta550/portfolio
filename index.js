31af894c31bacc21d115a5f8f79591604a4ca35b
Hamza Javed <softwareenginear786@gmail.com>
2021-01-14 17:43:39 +0500

    My Portfolio

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const app = express();
+const port = 9090 || process.env.PORT
+const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
+
+
+
+app.use(bodyParser.urlencoded({extended: false}))
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'hj6249314@gmail.com',
+    pass: 'hamza@786'
+  }
+});
+
+app.use(express.static(__dirname + '/src'));
+
+app.get("/",(req,res)=> {
+    res.sendFile(__dirname +"/src/index.html");
+})
+
+app.post("/send/email",(req,res)=> {
+    const { name, email, subject, body } = req.body
+    var mailOptions = {
+        from: 'hj6249314@gmail.com',
+        to: 'azeztaimur7@gmail.com',
+        subject: subject,
+        text: `Name: ${name} \n from: ${email} \n body: ${body}`
+    };
+    transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+          console.log(error);
+        } else {
+          res.redirect("/")
+        }
+    })
+});
+
+
+app.get('*', (req,res)=> {
+    res.json({"Message": "Page Not Found"})
+})
+
+app.listen(port)
