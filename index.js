48455e63fb9b576233cdc6c386ff86ad5318228f
Hamza Javed <softwareenginear786@gmail.com>
2021-01-20 17:42:34 +0500

    remove extra code

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const sendmail = require('sendmail')();
 app.use(bodyParser.urlencoded({extended: false}))
 
 let transporter = nodemailer.createTransport({
-  service: 'gsmtp',
+  service: 'gmail',
   auth: {
       user: process.env.EMAIL,
       pass: process.env.PASSWORD
